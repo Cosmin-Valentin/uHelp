21c9d7e9676ad968c576f40c80090e6d32538de2
Cosmin-Valentin <cosminv.nicola@gmail.com>
2025-03-06 08:36:45 +0200

    Remove dark background css

--- resources/views/elements/back-button.blade.php
+++ resources/views/elements/back-button.blade.php
@@ -1,8 +1,8 @@
 <div class="flex justify-between">
-    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
         {{ $header }}
     </h2>
     <div class="float-right">
-        <a href="{{ route($section) }}" class="inline-flex items-center px-4 py-1 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Back</a>  
+        <a href="{{ route($section) }}" class="inline-flex items-center px-4 py-1 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Back</a>  
     </div>
 </div>

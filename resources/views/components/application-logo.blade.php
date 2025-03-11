d3782147bbd7dcc5107365187c60a61c45096c11
Cosmin-Valentin <cosminv.nicola@gmail.com>
2025-03-11 10:30:02 +0200

    Customer view responsive css

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -7,11 +7,11 @@
       <rect x="70" y="120" width="40" height="40"/>
     </g>
     <g stroke="gray" stroke-width="2" fill="none">
-      <polygon points="60,20 80,10 120,10 100,20"/>
+      <polygon points="60,20 80,10 130,10 110,20"/>
       <polygon points="60,60 80,50 80,10 60,20"/>
       <polygon points="60,110 80,100 80,50 60,60"/>
       <polygon points="60,160 80,150 80,100 60,110"/>
       <polygon points="110,20 130,10 130,50 110,60"/>
-      <polygon points="110,160 130,150 130,100 110,110"/>
+      <polygon points="110,160 130,150 130,110 110,120"/>
     </g>
 </svg>
